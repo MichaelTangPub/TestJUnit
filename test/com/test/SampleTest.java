ac87edc1f206dccf8fb0b22a0bba66adf32119c0
Michael Tang <tangyanh@cn.ibm.com>
2019-03-31 20:16:12 +0800

    Fix dependency.

--- test/com/test/SampleTest.java
+++ test/com/test/SampleTest.java
@@ -1,12 +1,9 @@
 package com.test;
 
 import org.junit.Test;
-import org.junit.Ignore;
 import static org.junit.Assert.assertEquals;
 
 public class SampleTest {
-
-    String message = "Robert";
 
     @Test
     public void testScenario1() {
